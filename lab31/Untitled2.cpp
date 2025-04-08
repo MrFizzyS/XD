288f5602e5e55c49562f662cf8b27907c08319f1
Fizzy <maidaniuk.vladyslav@chnu.edu.ua>
2025-04-08 21:31:23 +0300

    lab 31 dorobleno, 32

--- lab31/Untitled2.cpp
+++ lab31/Untitled2.cpp
@@ -12,6 +12,17 @@
 	    int age;
 	    
 	    public:
+    People() {}
+    
+    People(const People &other) 
+	{
+        surname = other.surname;
+        country = other.country;
+        gender = other.gender;
+        education = other.education;
+        birthYear = other.birthYear;
+        age=other.age;
+    }
     void input() 
 	{
         cout<<"Enter surname: ";
@@ -22,7 +33,7 @@
         cin>>education;
         cout<<"Enter birth year: ";
         cin>>birthYear;
-        age=2023-birthYear;
+        age=2025-birthYear;
     }
     void display() 
 	{
@@ -37,16 +48,36 @@
 		{
         return (education=="higher" || education=="Higher") && (age>min);
     }
-    string getGender() 
+    string genderr() 
 	{
 	 return gender; 
 	 }
 	};
+	
+	int removegender(People human[],int n,string gender1)
+	{
+        int newsize=0;
+        for(int i=0;i<n;i++){
+            if(human[i].genderr()!=gender1)
+			{
+                human[newsize]=human[i];
+                newsize++;
+            }
+        }
+    return newsize;
+}
+void print(People human[],int n,int aage)
+{
+    for(int i=0;i<n;i++){
+        if(human[i].is(aage))
+            human[i].display();
+    }
+}
 	int main()
 	{
 		int maxsize=100;
     People people[100];
-    int size=0;
+    int size=0,age;
 	cout<<"Enter number of people: ";
     cin>>size;
     for (int i=0;i<size;i++) 
@@ -54,7 +85,37 @@
         cout<<"Enter data for person "<<i+1<<endl;
         people[i].input();
     }
+    cout<<"Enter minimum age for higher education filter: ";
+    cin>>age;
+    cout << "People with higher education and age above " << age << ":" << endl;
+    print(people, size, age);
+    cout<<endl;
+    cout<<endl;
+    string gender2;
+    cout<<"Enter gender to remove: ";
+    cin>>gender2;
+    size=removegender(people,size,gender2);
+    for(int i=0;i<size;i++)
+	{
+        people[i].display();
+    }	
 		
-		
+ 		    if (size>0) 
+			{
+        People copiedPerson(people[0]);
+        
+        for(int i=size;i>0;i--) 
+		{
+            people[i]=people[i-1];
+        }
+        people[0]=copiedPerson;
+        size++;
+        
+        cout<<"After inserting copied person at the beginning:"<<endl;
+        for(int i = 0; i<size; i++) 
+		{
+            people[i].display();
+        }
+    }
 		return 0;
 	}
